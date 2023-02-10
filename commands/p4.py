b27a26683e370b76601faf1930648b92d0124225
Leroy <leroyhaveamailbox@gmail.com>
2023-02-10 21:26:17 +0100

    working on test function

--- commands/p4.py
+++ commands/p4.py
@@ -5,6 +5,22 @@ import discord
 from discord.ext.commands import Context, Bot
 
 from functions.embeding import embed_help
+from puissance4.puissance4 import User, AI, game
+
+if not os.path.exists("puissance4/ai") :
+    os.mkdir("puissance4/ai")
+
+AIs = {}
+
+for file in os.listdir("puissance4/ai/") :
+    name = '.'.join(file.split('.')[:-1])
+    ext = file.split('.')[-1]
+    if set(c for c in name) == {str(i) for i in range(10)} :
+        AIs[name] = ext
+
+
+async def ask_move(board: list[list[int]], user: discord.User, channel: discord.TextChannel) :
+    pass
 
 async def command_(bot: Bot, ctx: Context, *args: str) :
     n_args = len(args)
@@ -31,14 +47,14 @@ async def command_(bot: Bot, ctx: Context, *args: str) :
         raw_submission = get(attached_file_url).text
 
         ext = files[0].filename.split('.')[-1]
-        name = ctx.message.author.name + '.' + ext
+        name = ctx.message.author.id
 
         replace = True
 
         if not os.path.exists("puissance4/ai") :
             os.mkdir("puissance4/ai")
 
-        if os.path.exists(f"puissance4/ai/{name}") :
+        if name in AIs :
             await ctx.channel.send("You already have a submission, do you want to replace it? (Y/N)")
 
             while True :
@@ -50,8 +66,11 @@ async def command_(bot: Bot, ctx: Context, *args: str) :
                     replace = False
                     break
             
+        else :
+            AIs[name] = ext
+            
         if replace :
-            File = open(f"puissance4/ai/{name}", 'w')
+            File = open(f"puissance4/ai/{name}.{ext}", 'w')
             File.write(raw_submission)
             File.close()
 
@@ -59,3 +78,24 @@ async def command_(bot: Bot, ctx: Context, *args: str) :
         
         else :
             await ctx.channel.send("Okie Dokie !")
+
+        return
+
+    if args[0] == "test" :
+        name = ctx.message.author.id
+        
+        if name not in AIs :
+            await ctx.channel.send("You don't have an AI! Upload one by messaging me `!p4 submit`.")
+            return
+        
+        dm = await ctx.message.author.create_dm()
+
+        ext = AIs[name]
+        if "self" in args :
+            p1 = AI(f"puissance4/ai/{name}.{ext}")
+            p2 = User(ask_move, ctx.message.author, ctx.channel)
+            if "first" in args :
+                p1, p2 = p2, p1
+        
+        
+        
