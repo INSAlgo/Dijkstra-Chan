0a096956c07524f8aa135a54ac31cc24487f266d
Lysquid <romainbenoit21560@gmail.com>
2024-03-26 22:37:18 +0100

    challenge leaderboard

--- cogs/code_golf.py
+++ cogs/code_golf.py
@@ -3,11 +3,10 @@ import pathlib
 import discord
 import discord.ext.commands as cmds
 import requests
-import os
 
 from main import CustomBot
 from utils.checks import in_channel
-from utils.ids import *
+from utils import ids
 
 
 class CodeGolf(cmds.Cog, name="Code golf"):
@@ -51,6 +50,7 @@ class CodeGolf(cmds.Cog, name="Code golf"):
         if not challenge_path.is_dir():
             challenge_path.mkdir(parents=True)
         
+        # TODO only warn of replacement if the new submission is not shorter
         for previous_file in challenge_path.glob(f"{name}.*"):
             previous_file.unlink()
             await ctx.send("Your previous submission will be replaced")
@@ -61,8 +61,27 @@ class CodeGolf(cmds.Cog, name="Code golf"):
         
         bytes = file.stat().st_size
         characters = len(program)
+        # TODO send a message in the code golf channel if it is a new record
 
         await ctx.send(f"Program submitted! <:feelsgood:737960024390762568> {bytes} bytes / {characters} characters")
+
+    @golf.command()
+    async def leaderboard(self, ctx: cmds.Context, challenge: challenge = None):
+        """
+        Display the leaderboard of the code golf
+        """
+
+        embed = discord.Embed(title=f"Code golf leaderboard")
+        if challenge is not None:
+            challenge_path = CodeGolf.FILES_PATH / str(challenge)
+            leaderboard = [(file.stat().st_size, file.stem) for file in challenge_path.iterdir()]
+            leaderboard.sort()
+            leaderboard = "\n".join(f"{user} : {size} bytes" for size, user in leaderboard)
+            embed.add_field(name=f"Challenge {challenge}", value=leaderboard, inline=False)
+        else:
+            embed.add_field(name=f"All challenges", value="TODO", inline=False) 
+
+        await ctx.send(embed=embed)
 
 async def setup(bot: CustomBot):
     await bot.add_cog(CodeGolf(bot))
